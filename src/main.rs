7a90396f6528df9940ace54cf5029a1007779e18
Moss Mayfly <mossmayfly@proton.me>
2026-07-15 15:18:58 -0400

    Moved helpers, and added frame by frame seeking

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,27 @@ use iced::futures::stream::Skip;
 use iced::widget::{container, stack};
 use iced::{advanced::graphics::futures::backend::default, widget::column};
 use iced::{Element, Event, Size, Subscription, event, window};
-use iced_video_player::{Video, VideoPlayer};
+use iced_video_player::{Position, Video, VideoPlayer};
 
 use std::io::IoSlice;
 use std::ops::Not;
+use std::str::FromStr;
 use std::time::Duration;
 use std::{default::Default, path::PathBuf};
 
 use log::{debug, info, warn, error};
+
+mod helpers {
+    use std::time::Duration;
+
+    pub fn safe_duration_sub(d1: Duration, d2: Duration) -> Duration {
+        if d2 > d1 {
+            return Duration::from_secs(0);
+        } else {
+            return d1 - d2;
+        }
+    }
+}
 
 enum PlayState {
     Pause,
@@ -23,20 +36,12 @@ enum Message {
     Pause(iced_video_player::Position),
     Play(iced_video_player::Position),
     SkipTo(iced_video_player::Position),
-    SkipForwardBy(Duration),
-    SkipBackwardBy(Duration),
+    SkipForwardBy(iced_video_player::Position),
+    SkipBackwardBy(iced_video_player::Position),
     ToggleLoop,
     TogglePlay,
 
     WindowResize(iced::Size),
-}
-
-mod helpers {
-    use std::time::Duration;
-
-    // fn position_as_frame(time: Duration, framerate: f64) -> u32 {
-    //     (time*framerate). as u32
-    // }
 }
 
 struct App {
@@ -49,8 +54,8 @@ impl Default for App {
         info!("Opening file passed to program.");
         Self { window_size: Default::default(), video: Video::new(
                 &url::Url::from_file_path(
-                    std::env::args().skip(1).collect::<Vec<String>>()[0].as_str()
-                ).unwrap()
+                            std::env::args().skip(1).collect::<Vec<String>>()[0].as_str()
+                        ).unwrap()
             ).unwrap()
         }
     }
@@ -58,7 +63,9 @@ impl Default for App {
 
 impl App {
     fn new() -> Self {
-        Self::default()
+        let out = Self::default();
+        info!("Current video framerate: {}", out.video.framerate());
+        out
     }
 
     fn run(size: iced::Size) {
@@ -78,11 +85,15 @@ impl App {
                 iced::event::Event::Keyboard(iced::keyboard::Event::KeyPressed { key, ..}) => match key {
                     iced::keyboard::key::Key::Character(c) => match c.as_str() {
                         "l" => Some(Message::ToggleLoop),
+                        "," => Some(Message::SkipBackwardBy(Position::Frame(1))), // watching the "<" key, but it needs to be checking the "," for programming reasons
+                        "." => Some(Message::SkipForwardBy(Position::Frame(1))), // watching the ">" key, but it needs to be checking the "." for programming reasons
                         _ => None
                     },
                     iced::keyboard::Key::Named(iced::keyboard::key::Named::Space) => Some(Message::TogglePlay),
-                    iced::keyboard::Key::Named(iced::keyboard::key::Named::ArrowLeft) => Some(Message::SkipBackwardBy(Duration::from_secs(5))),
-                    iced::keyboard::Key::Named(iced::keyboard::key::Named::ArrowRight) => Some(Message::SkipForwardBy(Duration::from_secs(5))),
+                    iced::keyboard::Key::Named(iced::keyboard::key::Named::ArrowLeft) => Some(Message::SkipBackwardBy(Position::Time(Duration::from_secs(5)))),
+                    iced::keyboard::Key::Named(iced::keyboard::key::Named::ArrowRight) => Some(Message::SkipForwardBy(Position::Time(Duration::from_secs(5)))),
+                    // iced::keyboard::Key::Named(iced::keyboard::key::Named::) => Some(Message::SkipBackwardBy(Position::Time(Duration::from_secs(5)))),
+                    // iced::keyboard::Key::Named(iced::keyboard::key::Named::ArrowRight) => Some(Message::SkipForwardBy(Position::Time(Duration::from_secs(5)))),
                     _ => None
                 },
                 _ => None
@@ -114,15 +125,27 @@ impl App {
                 }
             },
 
-            SkipForwardBy(dur) => {
+            SkipForwardBy(Position::Time(dur)) => {
                 if let Err(_) = self.video.seek(self.video.position()+dur, true) {
-                    error!("Could not seek forward by {dur:?}")
+                    error!("Could not seek forward by {dur:?}s")
                 }
             },
 
-            SkipBackwardBy(dur) => {
-                if let Err(_) = self.video.seek(self.video.position()-dur, true) {
-                    error!("Could not seek backward by {dur:?}")
+            SkipBackwardBy(Position::Time(dur)) => {
+                if let Err(_) = self.video.seek(helpers::safe_duration_sub(self.video.position(), dur), true) {
+                    error!("Could not seek backward by {dur:?}s")
+                }
+            },
+
+            SkipForwardBy(Position::Frame(f)) => {
+                if let Err(_) = self.video.seek(self.video.position() + Duration::from_millis((1000.0/self.video.framerate()) as u64), true) {
+                    error!("Could not seek forward by {f} frame")
+                }
+            },
+
+            SkipBackwardBy(Position::Frame(f)) => {
+                if let Err(_) = self.video.seek(helpers::safe_duration_sub(self.video.position(), Duration::from_millis((1000.0/self.video.framerate()) as u64)), true) {
+                    error!("Could not seek backward by {f} frame")
                 }
             },
 
